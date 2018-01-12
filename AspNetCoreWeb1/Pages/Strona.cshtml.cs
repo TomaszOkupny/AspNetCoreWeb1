a1dadb539c08e8efd6b0e5c88074099f78f2d403
Tomasz Okupny <foto@tomaszokupny.pl>
2018-01-12 12:51:24 +0100

    1

---
+++ AspNetCoreWeb1/Pages/Strona.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreWeb1.Pages
+{
+    
+
+    public class StronaModel : PageModel
+    {
+        public string Message { get; set; }
+
+        public void OnGet()
+        {
+            Message = "To jest nowa strona";
+        }
+    }
+}
